9d0f9b3810ff92abf0ab576b7b16bf85201ca300
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-13 19:31:03 +0530

    added Scanner class and also delared a global static list and also detected the drives in system using the list.Roots()

--- multi-drive-file-search/src/File_searcher/ file_searcher.java
+++ multi-drive-file-search/src/File_searcher/ file_searcher.java
@@ -10,4 +10,13 @@ public class file_searcher
 
   public static void main(String[] args)
   {
-     
+      try (Scanner input = new Scanner(System.in))
+      {
+          System.out.println("detecting drives in your system...");
+          File[] drives = File.listRoots();
+          System.out.println("drives detected in your system:");
+          if (drives == null || drives.length == 0)
+          {
+              System.out.println("no drives found.");
+              return;
+          }
