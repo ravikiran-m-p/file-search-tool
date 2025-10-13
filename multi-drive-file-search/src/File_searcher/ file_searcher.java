34bf9134b17844643795dd259903891b58bc470c
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-13 19:29:55 +0530

    ADDED A CLASS ADN ADN ASLO main method

--- multi-drive-file-search/src/File_searcher/ file_searcher.java
+++ multi-drive-file-search/src/File_searcher/ file_searcher.java
@@ -3,3 +3,11 @@ package File_searcher;
 import java.io.File;
 import java.util.*;
 import java.util.concurrent.*;
+
+public class file_searcher
+{
+  private static final List<File> found_files = Collections.synchronizedList(new ArrayList<>());
+
+  public static void main(String[] args)
+  {
+     
